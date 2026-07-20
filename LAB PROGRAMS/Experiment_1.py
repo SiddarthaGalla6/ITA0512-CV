6b7bcf322fa2a38d26a1d12eb05d4fe52490340d
SiddarthaGalla6 <192425093.simats@saveetha.com>
2026-07-20 10:08:15 +0530

    Update Experiment_1.py

--- LAB PROGRAMS/Experiment_1.py
+++ LAB PROGRAMS/Experiment_1.py
@@ -1,1 +1,6 @@
-
+import cv2
+image = cv2.imread("C:/Users/sidda/Downloads/loki wallpaper.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray Image", gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
