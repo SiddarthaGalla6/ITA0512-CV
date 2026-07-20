9ac9600283d5dd0db0dfef4188396e1760972835
SiddarthaGalla6 <192425093.simats@saveetha.com>
2026-07-20 10:02:45 +0530

    Update CO1_AT1_Q3.py

--- ASSIGNMENT/CO1_AT1_Q3.py
+++ ASSIGNMENT/CO1_AT1_Q3.py
@@ -1,1 +1,8 @@
-
+import cv2
+img = cv2.imread = cv2.imread("C:/Users/sidda/Downloads/loki wallpaper.jpg")
+h, w = img.shape[:2]
+small = cv2.resize(img, (w//5,h//5))
+jagged = cv2.resize(small,(w, h), interpolation = cv2.INTER_NEAREST)
+cv2.imshow("Low Sampling", jagged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
