edbde04532d0e9aba0c6611eeabd61e41a67f993
SiddarthaGalla6 <192425093.simats@saveetha.com>
2026-07-20 10:14:24 +0530

    Create Experiment_4.py

---
+++ LAB PROGRAMS/Experiment_4.py
@@ -0,0 +1,7 @@
+import cv2
+img = cv2.imread("C:/Users/sidda/Downloads/loki wallpaper.jpg", 0)
+equalized = cv2.equalizeHist(img)
+cv2.imshow("Original", img)
+cv2.imshow("Equalized", equalized)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
