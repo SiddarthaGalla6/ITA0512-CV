04860c51486cabd6fc0eb0443529be3b5f1a3312
SiddarthaGalla6 <192425093.simats@saveetha.com>
2026-08-05 06:24:49 +0530

    Create Experiment_24.py

---
+++ LAB PROGRAMS/Experiment_24.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+image = cv2.imread("C:/Users/sidda/Downloads/Thor.jpg")
+kernel = np.ones((5,5), np.uint8)
+blackhat = cv2.morphologyEx(image, cv2.MORPH_BLACKHAT, kernel)
+cv2.imshow("Original Image", image)
+cv2.imshow("Black Hat", blackhat)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
