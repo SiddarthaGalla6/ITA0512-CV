53c82c0106342551557a7a1ec64af3b6165c5ab9
SiddarthaGalla6 <192425093.simats@saveetha.com>
2026-08-07 11:17:14 +0530

    Create CO2_AT2_Edge Enhancement.py

---
+++ ASSIGNMENT/CO2/CO2_AT2_Edge Enhancement.py
@@ -0,0 +1,15 @@
+import cv2
+import matplotlib.pyplot as plt
+image = cv2.imread("sample.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 100, 200)
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.title("Original Image")
+plt.axis("off")
+plt.subplot(1,2,2)
+plt.imshow(edges, cmap="gray")
+plt.title("Edge Enhanced Image")
+plt.axis("off")
+plt.show()
